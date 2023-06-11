c883354eade642f4df61f375b7c763a9115f41ca
Hensvik <872304091@qq.com>
2023-06-11 19:37:58 +0800

    20230611

--- src/test/java/cn/itcast/nio/c4/TestWriteServer.java
+++ src/test/java/cn/itcast/nio/c4/TestWriteServer.java
@@ -5,12 +5,34 @@ import lombok.extern.slf4j.Slf4j;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
-import java.nio.channels.*;
-import java.nio.charset.Charset;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
 import java.util.Iterator;
 
+import static cn.itcast.nio.c2.ByteBufferUtil.debugAll;
+
 @Slf4j
-public class Server {
+public class TestWriteServer {
+    private static void split(ByteBuffer source) {
+        source.flip();
+        for (int i = 0; i < source.limit(); i++) {
+            // 找到一条完整消息
+            // 如果遇到了 \n 的换行符
+            if (source.get(i) == '\n') {
+                int length = i + 1 - source.position();
+                // 把这条完整消息存入新的 ByteBuffer
+                ByteBuffer target = ByteBuffer.allocate(length);
+                // 从 source 读，向 target 写
+                for (int j = 0; j < length; j++) {
+                    target.put(source.get());
+                }
+                debugAll(target);
+            }
+        }
+        source.compact();
+    }
 
     public static void main(String[] args) throws IOException {
         // 1. 创建 selector, 管理多个 channel
@@ -44,7 +66,10 @@ public class Server {
                     SocketChannel sc = channel.accept(); //阻塞方法
                     sc.configureBlocking(false);    //将socketChannel设置为非阻塞
 
-                    SelectionKey scKey = sc.register(selector, 0, null);
+                    //将buffer新建放在这个位置，可以单独为每个channel创建buffer附件
+                    // 将一个byteBuffer 作为附件放到selectionKey上
+                    ByteBuffer buffer = ByteBuffer.allocate(16); //分配buffer 4个字节
+                    SelectionKey scKey = sc.register(selector, 0, buffer);
                     //设置关注读事件
                     scKey.interestOps(SelectionKey.OP_READ);
                     log.debug("{}", sc);
@@ -53,14 +78,26 @@ public class Server {
                     //这里使用try catch的原因是为了不让客户端的异常关闭影响到服务器端
                     try {
                         SocketChannel channel = (SocketChannel) key.channel(); // 拿到触发事件的channel
-                        ByteBuffer buffer = ByteBuffer.allocate(4); //分配buffer 4个字节
+
+                        //获取byteBuffer上的附件
+                        ByteBuffer buffer = (ByteBuffer) key.attachment();
                         int read = channel.read(buffer); // 如果是正常断开，read 的方法的返回值是 -1
                         if(read == -1) {
                             key.cancel();
                         } else {
-                            buffer.flip();
+                            /*buffer.flip();
 //                            debugAll(buffer);
-                            System.out.println(Charset.defaultCharset().decode(buffer));
+                            System.out.println(Charset.defaultCharset().decode(buffer));*/
+
+                            split(buffer);
+                            //如果空间用完，则进行扩容
+                            if(buffer.position() == buffer.limit()){
+                                ByteBuffer newBuffer = ByteBuffer.allocate(buffer.capacity()*2);
+                                //切换模式
+                                buffer.flip();
+                                newBuffer.put(buffer);
+                                key.attach(newBuffer);
+                            }
                         }
                     } catch (IOException e) {
                         e.printStackTrace();
