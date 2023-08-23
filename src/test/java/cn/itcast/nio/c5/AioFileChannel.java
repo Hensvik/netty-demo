19c49cb39550803c175f3e0b7bffaa3c3d4c1e43
Hensvik <872304091@qq.com>
2023-08-23 23:16:39 +0800

    20230823

--- src/test/java/cn/itcast/nio/c5/AioFileChannel.java
+++ src/test/java/cn/itcast/nio/c5/AioFileChannel.java
@@ -11,6 +11,7 @@ import java.nio.file.StandardOpenOption;
 
 import static cn.itcast.nio.c2.ByteBufferUtil.debugAll;
 
+//异步体现在，read begin后就打印read end，表示此时就进行异步等待结果
 @Slf4j
 public class AioFileChannel {
     public static void main(String[] args) throws IOException {
@@ -37,6 +38,6 @@ public class AioFileChannel {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        System.in.read();
+        System.in.read();   //必须保持主线程挂起，因为用来打印结果的线程是守护线程，因此主线程结束后守护线程就强行中断了
     }
 }
